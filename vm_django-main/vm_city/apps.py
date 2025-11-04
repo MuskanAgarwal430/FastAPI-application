d8f50f42a7150a23b7c1dd0dce06f4e4c23741b2
Muskan Agarwal <98253687+MuskanAgarwal430@users.noreply.github.com>
2025-11-04 14:51:15 +0530

    Delete vm_django-main directory

--- vm_django-main/vm_city/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VmCityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vm_city'
--- vm_django-main/vm_city_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import  path
-from .views import IssueCreateAPIView, SyncIssuePartsView
-
-
-urlpatterns = [
-    path('issue/', IssueCreateAPIView.as_view(), name="firebase"),
-    path('sync-issue-parts/', SyncIssuePartsView.as_view(), name='sync-issue-parts'),
-]
